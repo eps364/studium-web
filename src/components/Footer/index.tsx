cfd978a3b9258f582ef35cd0addbf520781fa107
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2021-09-11 04:36:56 -0300

    fix(Fix AppLink): href and label

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,8 @@
-import BottomNavigation from '@material-ui/core/BottomNavigation'
-import { makeStyles } from '@material-ui/core/styles'
-import Typography from '@material-ui/core/Typography'
-import React from 'react'
-import AppLink from '../../components/AppLink'
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
+import AppLink from '../../components/AppLink';
 
 const useStyles = makeStyles({
     root: {
@@ -11,10 +11,10 @@ const useStyles = makeStyles({
         height: 50,
         bottom: 0,
     },
-})
+});
 
 export default function Footer(): JSX.Element {
-    const classes = useStyles()
+    const classes = useStyles();
     return (
         <BottomNavigation className={classes.root}>
             <Typography variant="body2" color="textSecondary" align="center">
@@ -22,12 +22,11 @@ export default function Footer(): JSX.Element {
 
                 <AppLink
                     label={process.env.NEXT_PUBLIC_NAME}
-                    color="inherit"
                     href="https://studium.blog.br"
                 />
                 {new Date().getFullYear()}
                 {'.'}
             </Typography>
         </BottomNavigation>
-    )
+    );
 }
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,19 +1,19 @@
-import { Box } from '@material-ui/core'
-import Avatar from '@material-ui/core/Avatar'
-import Button from '@material-ui/core/Button'
-import Checkbox from '@material-ui/core/Checkbox'
-import Container from '@material-ui/core/Container'
-import FormControl from '@material-ui/core/FormControl'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import FormGroup from '@material-ui/core/FormGroup'
-import FormLabel from '@material-ui/core/FormLabel'
-import Grid from '@material-ui/core/Grid'
-import { makeStyles } from '@material-ui/core/styles'
-import TextField from '@material-ui/core/TextField'
-import Typography from '@material-ui/core/Typography'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
-import React from 'react'
-import AppLink from '../components/AppLink'
+import { Box } from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import Container from '@material-ui/core/Container';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormLabel from '@material-ui/core/FormLabel';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import React from 'react';
+import AppLink from '../components/AppLink';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -33,10 +33,10 @@ const useStyles = makeStyles((theme) => ({
     submit: {
         margin: theme.spacing(3, 0, 2),
     },
-}))
+}));
 
 export default function SignUp() {
-    const classes = useStyles()
+    const classes = useStyles();
 
     return (
         <Container component="main" maxWidth="xs">
@@ -123,7 +123,6 @@ export default function SignUp() {
                             <Grid item>
                                 <AppLink
                                     href="login"
-                                    variant="body2"
                                     label="Already have an account? Sign in"
                                 />
                             </Grid>
@@ -132,5 +131,5 @@ export default function SignUp() {
                 </FormControl>
             </Box>
         </Container>
-    )
+    );
 }

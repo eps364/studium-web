8998c9dd059e88f28596abfafb4b298a807a69b1
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2021-09-11 02:58:09 -0300

    feat(design): Add login

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,20 +1,25 @@
-import BottomNavigation from '@material-ui/core/BottomNavigation'
-import { makeStyles } from '@material-ui/core/styles'
-import Typography from '@material-ui/core/Typography'
-import React from 'react'
+import { Link } from '@material-ui/core';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
 
 const useStyles = makeStyles({
     root: {},
-})
+});
 
 export default function Footer(): JSX.Element {
-    const classes = useStyles()
-
+    const classes = useStyles();
     return (
         <BottomNavigation className={classes.root}>
-            <Typography variant="h6" color="inherit">
-                Teste
+            <Typography variant="body2" color="textSecondary" align="center">
+                {'Copyright © '}
+                <Link color="inherit" href="https://studium.blog.br">
+                    <a>{process.env.NEXT_PUBLIC_NAME}</a>
+                </Link>{' '}
+                {new Date().getFullYear()}
+                {'.'}
             </Typography>
         </BottomNavigation>
-    )
+    );
 }
---
+++ src/pages/login.tsx
@@ -0,0 +1,113 @@
+import { Box } from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import Container from '@material-ui/core/Container';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormLabel from '@material-ui/core/FormLabel';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { Email, GitHub } from '@material-ui/icons';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import React from 'react';
+
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        borderRadius: '50%',
+        backgroundColor: theme.palette.primary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+export default function SignIn() {
+    const classes = useStyles();
+    return (
+        <Container component="main" maxWidth="xs">
+            <Box className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <FormControl component="form">
+                    <FormLabel>Login</FormLabel>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        <Email className={classes.avatar} />
+                        Sign In
+                    </Button>
+
+
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        <GitHub className={classes.avatar} />
+                        Login with GitHub
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </FormControl>
+            </Box>
+        </Container>
+    );
+}
